28d026f447c3efbc60b90696b07cba9790d8703d
briantical <lutbrianivan@gmail.com>
2020-04-10 03:08:15 +0300

    Removed consoles

--- src/estimator.js
+++ src/estimator.js
@@ -102,18 +102,4 @@ const covid19ImpactEstimator = (data) => {
   return { data, impact, severeImpact };
 };
 
-console.log(covid19ImpactEstimator({
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 4,
-    avgDailyIncomePopulation: 0.73
-  },
-  periodType: 'days',
-  timeToElapse: 38,
-  reportedCases: 2747,
-  population: 92931687,
-  totalHospitalBeds: 678874
-}));
-
-// export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
