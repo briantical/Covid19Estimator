5249ef0390566b177a0e480a374eccfb3b99a957
briantical <lutbrianivan@gmail.com>
2020-04-12 01:02:57 +0300

    Configuring Netlify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const express = require('express');
+const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const servefavicon = require('serve-favicon');
-const path = require('path');
 
-const logger = require('./src/logger');
-const api = require('./src/routes');
+
+const logger = require('./logger');
+const api = require('./routes');
 
 const PORT = 3000;
 
@@ -15,14 +17,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Define the template engine to use
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, '/src/views'));
+app.set('views', path.join(__dirname, '/views'));
 
 // Using the favicons
-app.use(servefavicon(path.join(__dirname, '/public/favicon/favicon.ico')));
+app.use(servefavicon(path.join(__dirname, './../public/favicon/favicon.ico')));
 
 // Access the public folder via static route
-app.use('/static', express.static(path.join(__dirname, '/public')));
+app.use('/static', express.static(path.join(__dirname, './../public')));
 
 app.use('/', api);
 
 app.listen(PORT, () => logger.info('Covid19 Estimator started at ', new Date().toJSON()));
+
+module.exports = app;
+module.exports.handler = serverless(app);
